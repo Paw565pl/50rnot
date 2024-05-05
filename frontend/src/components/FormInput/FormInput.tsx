f2961de14ca39bf0e76a4dbd80fca71bfeac02e4
Paw565pl <paw565pl@gmail.com>
2024-05-05 18:37:42 +0200

    frontend: create form input component

---
+++ frontend/src/components/FormInput/FormInput.tsx
@@ -0,0 +1,21 @@
+import { InputHTMLAttributes, ReactNode } from "react";
+
+interface FormInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: ReactNode;
+}
+
+const FormInput = ({ label, ...props }: FormInputProps) => {
+  return (
+    <label className="form-control w-full">
+      <div className="label">
+        <span className="label-text">{label}</span>
+      </div>
+      <input
+        {...props}
+        className="input input-bordered w-full focus:outline-none focus:border-primary"
+      />
+    </label>
+  );
+};
+
+export default FormInput;
