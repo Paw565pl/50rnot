4a1a9b843667e6c6bd4590d3b3a94628c5126f9f
Paw565pl <paw565pl@gmail.com>
2024-05-05 18:26:51 +0200

    frontend: modify navbar style

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -2,9 +2,11 @@ import ThemeSwitch from "./ThemeSwitch";
 
 const NavBar = () => {
   return (
-    <nav className="navbar bg-primary text-primary-content md:px-36 xl:px-80 flex justify-between">
-      <div className="text-3xl font-bold">50rnot</div>
-      <ThemeSwitch />
+    <nav className="navbar bg-primary text-primary-content mb-10">
+      <div className="container mx-auto lg:w-1/2 flex justify-between">
+        <span className="text-3xl font-bold">50rnot</span>
+        <ThemeSwitch />
+      </div>
     </nav>
   );
 };
