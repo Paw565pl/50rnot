57784b3698a87793d9ba8bf31a88285ebeded1f2
Paw565pl <paw565pl@gmail.com>
2024-05-06 19:33:37 +0200

    frontend: fix empty string to number coercion

--- frontend/src/schemas/formSchema.ts
+++ frontend/src/schemas/formSchema.ts
@@ -1,22 +1,22 @@
 import { z } from "zod";
 
+const zodStringNumber = (fieldName: string) =>
+  z
+    .string()
+    .refine((value) => value.trim().length > 0, `${fieldName} is required`)
+    .refine((value) => !isNaN(Number(value)), `${fieldName} must be a number`)
+    .transform(Number)
+    .refine(
+      (value) => Number.isInteger(value),
+      `${fieldName} must be an integer`
+    )
+    .refine((value) => value >= 0, `${fieldName} must be a nonnegative number`);
+
 const formSchema = z.object({
-  age: z
-    .number({
-      required_error: "Age is required",
-      invalid_type_error: "Age must be a number",
-    })
-    .int("Age must be an integer")
-    .nonnegative("Age must be a nonnegative number"),
+  age: zodStringNumber("Age"),
   workclass: z.string({ required_error: "Workclass is required" }).trim(),
   education: z.string({ required_error: "Education level is required" }).trim(),
-  education_num: z
-    .number({
-      required_error: "Total years of education is required",
-      invalid_type_error: "Total years of education must be a number",
-    })
-    .int("Total years of education must be an integer")
-    .nonnegative("Total years of education must be a nonnegative number"),
+  education_num: zodStringNumber("Total years of education"),
   marital_status: z
     .string({ required_error: "Marital status is required" })
     .trim(),
@@ -26,27 +26,9 @@ const formSchema = z.object({
     .trim(),
   race: z.string({ required_error: "Race is required" }).trim(),
   sex: z.string({ required_error: "Sex is required" }).trim(),
-  capital_gain: z
-    .number({
-      required_error: "Capital gain is required",
-      invalid_type_error: "Capital gain must be a number",
-    })
-    .int("Capital gain must be an integer")
-    .nonnegative("Capital gain must be a nonnegative number"),
-  capital_loss: z
-    .number({
-      required_error: "Capital loss is required",
-      invalid_type_error: "Capital loss must be a number",
-    })
-    .int("Capital loss must be an integer")
-    .nonnegative("Capital loss must be a nonnegative number"),
-  hours_per_week: z
-    .number({
-      required_error: "Work hours per week is required",
-      invalid_type_error: "Work hours per week must be a number",
-    })
-    .int("Work hours per week must be an integer")
-    .nonnegative("Work hours per week must be a nonnegative number"),
+  capital_gain: zodStringNumber("Capital gain"),
+  capital_loss: zodStringNumber("Capital loss"),
+  hours_per_week: zodStringNumber("Work hours per week"),
   native_country: z
     .string({ required_error: "Native country is required" })
     .trim(),
