2b007e7b7fb4562402a48e5a828e32a1166a7b92
Paw565pl <paw565pl@gmail.com>
2024-05-07 20:02:29 +0200

    frontend: create tanstack query client

---
+++ frontend/src/components/Providers.tsx
@@ -0,0 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
+
+interface ProvidersProps {
+  children: ReactNode;
+}
+
+const queryClient = new QueryClient();
+
+const Providers = ({ children }: ProvidersProps) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default Providers;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import Providers from "./components/Providers.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Providers>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </Providers>
   </React.StrictMode>
 );
