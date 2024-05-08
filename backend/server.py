6d6d8dc132f0e9fd37f059d713e29d01112a8ba5
Paw565pl <paw565pl@gmail.com>
2024-05-08 20:04:33 +0200

    backend: fix cors error

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from models import ModelInput, Prediction
 from use_model import make_prediction
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://frontend:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/predict")
