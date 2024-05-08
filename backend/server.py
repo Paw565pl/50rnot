45370f684a32b32bb772694b7b1e3f893178f908
Paw565pl <paw565pl@gmail.com>
2024-05-08 20:27:02 +0200

    backend: extend allowed origins list

--- backend/server.py
+++ backend/server.py
@@ -5,13 +5,17 @@ from use_model import make_prediction
 
 app = FastAPI()
 
+allowed_origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://frontend:5173",
+    "http://localhost:4173",
+    "http://127.0.0.1:4173",
+    "http://frontend:4173",
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://frontend:5173",
-    ],
+    allow_origins=allowed_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
