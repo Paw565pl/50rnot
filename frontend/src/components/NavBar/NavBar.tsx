cf9201e571ab199bca498b74476c4b1844d8b021
Paw565pl <paw565pl@gmail.com>
2024-05-08 18:20:54 +0200

    frontend: add padding to navbar content

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -3,7 +3,7 @@ import ThemeSwitch from "./ThemeSwitch";
 const NavBar = () => {
   return (
     <nav className="navbar bg-primary text-primary-content">
-      <div className="container mx-auto lg:w-1/2 flex justify-between">
+      <div className="container mx-auto lg:w-1/2 flex justify-between pl-2">
         <span className="text-3xl font-bold">50rnot</span>
         <ThemeSwitch />
       </div>
