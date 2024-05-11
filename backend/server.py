95ca24e7ac41920063716b938367fa1e53fc1846
Paw565pl <paw565pl@gmail.com>
2024-05-11 21:14:19 +0200

    add docker files

--- backend/server.py
+++ backend/server.py
@@ -8,10 +8,12 @@ app = FastAPI()
 allowed_origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "http://frontend:5173",
+
     "http://localhost:4173",
     "http://127.0.0.1:4173",
-    "http://frontend:4173",
+
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
 ]
 app.add_middleware(
     CORSMiddleware,
